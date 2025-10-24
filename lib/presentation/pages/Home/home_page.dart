63fc60c4cad2866f66a84c496f40e54bad180975
Mamboo Lab <mamboolab@MacBook-Pro-de-Mamboo.local>
2025-10-24 18:14:36 +0100

    feat: Add native splash screen

--- lib/presentation/pages/Home/home_page.dart
+++ lib/presentation/pages/Home/home_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage
+    extends
+        StatelessWidget {
+  const HomePage({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomePage Page'),
+        title: const Text(
+          'HomePage Page',
+        ),
+      ),
+      backgroundColor: Color(
+        0XFF4CAF50,
       ),
       body: const Center(
-        child: Text('Welcome to the HomePage Page!'),
+        child: Text(
+          'Welcome to the HomePage Page!',
+        ),
       ),
     );
   }
